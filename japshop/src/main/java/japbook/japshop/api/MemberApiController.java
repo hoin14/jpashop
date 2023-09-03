3e5a709c53cfbbbeb43d5a1e31d12bfb15632da5
hoin14 <hoins@localhost>
2023-09-03 23:37:07 +0900

    회원등록, 수정, 조회 API

---
+++ japshop/src/main/java/japbook/japshop/api/MemberApiController.java
@@ -0,0 +1,108 @@
+package japbook.japshop.api;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import japbook.japshop.domain.Member;
+import japbook.japshop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+	private final MemberService memberService;
+	
+	@GetMapping("/api/v1/members")
+	public List<Member> memberV1(){
+		return memberService.findMembers();
+	}
+
+	@GetMapping("/api/v2/members")
+	public Result memberV2(){
+		List<Member> findMembers = memberService.findMembers();
+		List<MemberDto> collect = findMembers.stream()
+				   .map(m -> new MemberDto(m.getName()))
+				   .collect(Collectors.toList());
+
+		return new Result(collect.size(), collect);
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class Result<T>{
+		private int count;
+		private T data;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class MemberDto{
+		private String name;
+	}
+
+	
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+	
+	@PostMapping("/api/v2/members")
+	public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+		Member member = new Member();
+		member.setName(request.getName());
+		
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+	
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+			@PathVariable("id") Long id,
+			@RequestBody @Valid UpdateMemberRequest request) {
+		
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+	
+	@Data
+	static class UpdateMemberRequest{
+		private String name;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse{
+		private Long id;
+		private String name;
+	}
+	
+	@Data
+	static class CreateMemberRequest{
+		@NotEmpty
+		private String name;
+	}
+	
+	@Data
+	static class CreateMemberResponse{
+		private Long id;
+		
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
+	}
+	
+}
--- japshop/src/main/java/japbook/japshop/domain/Member.java
+++ japshop/src/main/java/japbook/japshop/domain/Member.java
@@ -3,12 +3,15 @@ package japbook.japshop.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,12 +22,13 @@ public class Member {
 	@Id @GeneratedValue
 	@Column(name = "member_id")
 	private Long id;
-	
+
 	private String name;
 	
 	@Embedded
 	private Address address;
-	
+
+	@JsonIgnore
 	@OneToMany(mappedBy = "member")
 	private List<Order> orders = new ArrayList<>();
 	

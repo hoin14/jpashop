1dbfd2eabf355647c4d76a6a2ed64403d78c835d
hoin14 <hoins@localhost>
2023-09-23 11:07:52 +0900

    주문조회 V3_1: 페이징과 한계 돌파 - default_batch_fetch_size

--- japshop/src/main/java/japbook/japshop/repository/OrderRepository.java
+++ japshop/src/main/java/japbook/japshop/repository/OrderRepository.java
@@ -116,5 +116,29 @@ public class OrderRepository {
 				 			"join fetch o.delivery d", Order.class
 				 			).getResultList();
 	 }
+
+	 public List<Order> findAllWithItem(){
+		 return em.createQuery(
+				 	"select distinct o from Order o" +
+				 	" join fetch o.member m" +
+				 	" join fetch o.delivery d" +
+				 	" join fetch o.orderItems oi" +
+				 	" join fetch oi.item i", Order.class)
+				 .setFirstResult(1)
+				 .setMaxResults(100)
+				 .getResultList();
+				 
+	 }
+	 
+	 public List<Order> findAllWithMemberDelivery(int offset, int limit){
+		 return em.createQuery(
+				 			"select o from Order o " + 
+				 			"join fetch o.member m " +
+				 			"join fetch o.delivery d", Order.class
+				 			)
+				 .setFirstResult(offset)
+				 .setMaxResults(limit)
+				 .getResultList();
+	 }
 	 
 }
